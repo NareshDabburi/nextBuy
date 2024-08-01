3ced9862002d32795bd96ef407c93601aafa8476
naresh0904 <nareshdabburi0904@gmail.com>
2024-08-02 03:15:34 +0530

    changed done to host this website

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,10 @@ __dirname = path.resolve(); // Set __dirname to current directory
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
