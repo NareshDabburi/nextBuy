3e23576fd87ef5776f8629a5aac248cd2059604a
naresh0904 <nareshdabburi0904@gmail.com>
2024-08-02 02:32:22 +0530

    deployment config

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,18 @@ app.get("/api/config/paypal", (req, res) =>
 __dirname = path.resolve(); // Set __dirname to current directory
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
 app.use(errorMiddleWare.notFound);
 app.use(errorMiddleWare.errorHandler);
 
