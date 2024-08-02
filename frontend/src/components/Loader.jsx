757f84159fb2494be2d453a4156ce55f712c0a3e
naresh0904 <nareshdabburi0904@gmail.com>
2024-08-02 14:24:49 +0530

    fixed loader

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,10 +1,21 @@
-import { Spinner } from "react-bootstrap";
 import React from 'react'
+import { Spinner } from 'react-bootstrap'
 
 const Loader = () => {
   return (
-    <Spinner animation="borded" role="status" style = {{width:"100px",height:"100px",margin:"auto",display:"block"}}/>
+    <Spinner
+      animation='border'
+      role='status'
+      style={{
+        width: '100px',
+        height: '100px',
+        margin: 'auto',
+        display: 'block',
+      }}
+    >
+      <span className='sr-only'></span>
+    </Spinner>
   )
 }
 
-export default Loader;
+export default Loader
